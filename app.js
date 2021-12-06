742a2818c050bfb0b18f70ae9822de60c6cfef7e
davidlippai <lippai28@gmail.com>
2021-12-06 18:55:50 +0100

    version of telefon 1.0.1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let lineOne = document.getElementById("line-one");
 let lineTwo = document.getElementById("line-two");
 let lineThree = document.getElementById("line-three")
 
-
+//MENU---------------------------------
 $("#hamburgerMenuBtn").click(function(){
   $("#menu-list").slideToggle("600");
   lineOne.classList.toggle("line-one-animation");
@@ -11,8 +11,15 @@ $("#hamburgerMenuBtn").click(function(){
 })
 
 
-
-
 $("#provinz").click(function(){
   $(".provinz-list").slideToggle("600");
+})
+
+
+//SOCIAL MEDIA ICONS --------------------
+
+let socialMediaBox = document.getElementById("social-media");
+
+socialMediaBox.addEventListener('click',function(){
+  socialMediaBox.classList.toggle("slideIn");
 })
